8ed672411742dc9889850c99cdd45e0219a66b64
CastleKen <202380191@psu.palawan.edu.phgit config --global user.name>
2024-08-22 23:47:15 +0800

    done

--- PS01_2_OddEven/Program.cs
+++ PS01_2_OddEven/Program.cs
@@ -10,6 +10,14 @@ namespace PS01_2_OddEven
     {
         static void Main(string[] args)
         {
+            if (int number % 2 == 0)
+            {
+                Console.WriteLine($"The number {number} is even.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is odd.");
+            }
         }
     }
 }
