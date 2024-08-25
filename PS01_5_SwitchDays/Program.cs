f1be689a64d25c988a604a59972f0bc6588c62bf
CastleKen <202380191@psu.palawan.edu.phgit config --global user.name>
2024-08-25 11:02:11 +0800

    done

--- PS01_5_SwitchDays/Program.cs
+++ PS01_5_SwitchDays/Program.cs
@@ -10,6 +10,56 @@ namespace PS01_5_SwitchDays
     {
         static void Main(string[] args)
         {
+            int day;
+
+            switch (day)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Monday");
+                        break;
+                    }
+
+                case 2:
+                    {
+                        Console.WriteLine("Tuesday");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Wednesday");
+                        break;
+                    }
+
+                case 4:
+                    {
+                        Console.WriteLine("Thursday");
+                        break;
+                    }
+
+                case 5:
+                    {
+                        Console.WriteLine("Friday");
+                        break;
+                    }
+
+                case 6:
+                    {
+                        Console.WriteLine("Saturday");
+                        break;
+                    }
+
+                case 7:
+                    {
+                        Console.WriteLine("Sunday");
+                        break;
+                    }
+
+                default:
+                    {
+                        Console.WriteLine("Invalid number of day!")
+                        break:
+                    }
         }
     }
 }
